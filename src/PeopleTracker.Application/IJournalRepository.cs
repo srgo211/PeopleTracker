88dcf9a143b8d0216d5f814cf97c7866074d67a8
novikovsn87@gmail.com <novikovsn87@gmail.com>
2025-04-12 17:57:46 +0400

    Интерфейс с CRUD для IJournalRepository

---
+++ src/PeopleTracker.Application/IJournalRepository.cs
@@ -0,0 +1,25 @@
+﻿using PeopleTracker.Domain.Entities;
+
+namespace PeopleTracker.Application;
+
+/// <summary>Интерфейс репозитория для журнала посещаемости</summary>
+public interface IJournalRepository
+{
+    /// <summary>Получить все записи</summary>
+    Task<IReadOnlyList<JournalRecord>> GetAllAsync();
+
+    /// <summary>Получить запись по Id</summary>
+    Task<JournalRecord?> GetByIdAsync(int id);
+
+    /// <summary>Получить записи по пользователю и дате</summary>
+    Task<JournalRecord?> GetByUserAndDateAsync(int userId, DateOnly date);
+
+    /// <summary>Добавить запись</summary>
+    Task AddAsync(JournalRecord record);
+
+    /// <summary>Обновить запись</summary>
+    Task UpdateAsync(JournalRecord record);
+
+    /// <summary>Удалить запись по Id</summary>
+    Task<bool> DeleteAsync(int id);
+}
