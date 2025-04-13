1a1eb4daaa73dc10d54a8a61616da0bdfbe55edf
novikovsn87@gmail.com <novikovsn87@gmail.com>
2025-04-13 22:56:01 +0400

    завели Telerik c Maui

--- PeopleTracker.MauiApp/MauiProgram.cs
+++ PeopleTracker.MauiApp/MauiProgram.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using Telerik.Maui.Controls.Compatibility;
+
 namespace PeopleTracker.MauiApp;
 
 public static class MauiProgram
@@ -9,6 +11,7 @@ public static class MauiProgram
         var builder = Microsoft.Maui.Hosting.MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseTelerik()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
